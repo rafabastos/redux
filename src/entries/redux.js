b347caa8ab34bee7870349223eb678592a558259
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-16 11:06:19 -0300

    configuracion de proyecto y impresion de datos del estado

---
+++ src/entries/redux.js
@@ -0,0 +1,41 @@
+import { createStore } from 'redux';
+
+const $form = document.getElementById('form');
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData($form);
+  const title = data.get('title');
+  console.log(title);
+}
+const initialState = [
+  {
+    "title": "Despacito",
+  },
+  {
+    "title": "One more time",
+  },
+  {
+    "title": "Echame la culpa",
+  }
+]
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
+const $container = document.getElementById('playlist');
+const playlist = store.getState();
+
+playlist.forEach((item)=> {
+    const template = document.createElement('p');
+    template.textContent = item.title;
+    $container.appendChild(template);
+})
+
+console.log(store.getState());
+
+
