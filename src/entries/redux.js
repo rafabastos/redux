578bb12bdd367ccc1be25cd6b75a62d4da2eaae4
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-16 11:56:37 -0300

    se agrega estados con input y se imprime en pantalla

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -4,37 +4,61 @@ const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-  event.preventDefault();
-  const data = new FormData($form);
-  const title = data.get('title');
-  console.log(title);
+    event.preventDefault();
+    const data = new FormData($form);
+    const title = data.get('title');
+    console.log(title);
+    //setea estado
+    store.dispatch({
+        type: 'ADD_SONG',
+        payload: {
+            title,
+        },
+    })
 }
 const initialState = [
-  {
-    "title": "Despacito",
-  },
-  {
-    "title": "One more time",
-  },
-  {
-    "title": "Echame la culpa",
-  }
+    {
+        "title": "Despacito",
+    },
+    {
+        "title": "One more time",
+    },
+    {
+        "title": "Echame la culpa",
+    }
 ]
 
+const reducer = function(state, action) {
+    switch (action.type) {
+        case 'ADD_SONG':
+            return [...state, action.payload]
+        default:
+            return state
+    }
+}
+
 const store = createStore(
-  (state) => state,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    reducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
+function render() {
+    const $container = document.getElementById('playlist');
+    const playlist = store.getState();
+    $container.innerHTML = '';
+    playlist.forEach((item)=> {
+        const template = document.createElement('p');
+        template.textContent = item.title;
+        $container.appendChild(template);
+    })
+}
+render();
 
-const $container = document.getElementById('playlist');
-const playlist = store.getState();
+function handleChange() {
+    render();
+}
 
-playlist.forEach((item)=> {
-    const template = document.createElement('p');
-    template.textContent = item.title;
-    $container.appendChild(template);
-})
+store.subscribe(handleChange)
 
 console.log(store.getState());
 
